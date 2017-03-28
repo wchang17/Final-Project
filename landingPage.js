452ce9c1c9b030104f1498756e43a25351f98172
wchang17 <wanrung.chang@gmail.com>
2017-03-27 21:26:16 -0400

    3/27 Final Project first draft
    Added images, and created html pages

---
+++ landingPage.js
@@ -0,0 +1,20 @@
+var slideIndex = 0;
+showSlides();
+
+$(document).ready(function(){
+	$('.mySlides').css("display", "none")
+	});
+
+	function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
