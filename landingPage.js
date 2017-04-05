563bebacf3ef079ef87effa2535081f7f68d5c9c
wchang17 <wanrung.chang@gmail.com>
2017-04-05 19:55:46 -0400

    edited comment

--- landingPage.js
+++ landingPage.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1}    
     slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
